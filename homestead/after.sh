1aa8a96ccf03d48c30d9374efa5a3c1458401cd8
Alejandro Hernandez Ferriz <ahferriz@gmail.com>
2017-06-21 14:01:43 -0500

    Script de aprovisionamiento de todas las aplicaciones.

--- homestead/after.sh
+++ homestead/after.sh
@@ -12,20 +12,45 @@ fi
 
 if [ -d /home/vagrant/claropay/api ]; then
 	echo 'Provisioning Claro Pay API'
+    cd /home/vagrant/claropay/api
+    composer install
+    composer dump-autoload
+    php artisan migrate --seed
+    php artisan key:generate
 fi
 
 if [ -d /home/vagrant/claropay/clientes ]; then
 	echo 'Provisioning Claro Pay Clientes'
+    cd /home/vagrant/claropay/clientes
+    composer install
+    composer dump-autoload
+    php artisan migrate --seed
+    php artisan key:generate
 fi
 
 if [ -d /home/vagrant/claropay/boveda ]; then
 	echo 'Provisioning Claro Pay Boveda'
+    cd /home/vagrant/claropay/boveda
+    composer install
+    composer dump-autoload
+    php artisan migrate --seed
+    php artisan key:generate
 fi
 
 if [ -d /home/vagrant/claropay/queue ]; then
 	echo 'Provisioning Claro Pay Queue'
+    cd /home/vagrant/claropay/queue
+    composer install
+    composer dump-autoload
+    php artisan migrate --seed
+    php artisan key:generate
 fi
 
 if [ -d /home/vagrant/claropay/monitor ]; then
 	echo 'Provisioning Claro Pay Monitor'
+    cd /home/vagrant/claropay/monitor
+    composer install
+    composer dump-autoload
+    php artisan migrate --seed
+    php artisan key:generate
 fi
