8dc6c2a7a827c6ae00a9e76408f7d254b38cfd3a
Alejandro Hernandez Ferriz <ahferriz@gmail.com>
2017-12-06 10:13:53 -0600

    Homologación de seeder de transacciones. Archivo faltante de migration: activity_log_table.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(TransaccionSeeder::class);
+         $this->call(TransaccionTableSeeder::class);
     }
 }
---
+++ database/seeds/TransaccionTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Transaccion;
+
+class TransaccionTableSeeder extends Seeder
+{
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Inserta valores iniciales
+//        Transaccion::create([
+//            'uuid' => '5b948b52-7ffb-4fbd-beff-143a0f319d9b',
+//            'comercio_id' => 1,
+//            'prueba' => false,
+//            'operacion' => 'preautorizacion',
+//            'transaccion_estatus_id' => 3,
+//            'pais_id' => 1,
+//            'moneda_id' => 1,
+//            'monto' => 56900,
+//            'forma_pago' => 'paypal',
+//            'datos_pago' => '{}',
+//            'datos_antifraude' => '{}',
+//            'datos_comercio' => '{}',
+//            'datos_claropagos' => '{}',
+//            'datos_procesador' => '{}',
+//            'datos_destino' => '{}'
+//
+//        ]);
+    }
+}
