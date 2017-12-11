0ba988cdda5d46b91ccc5bf931db7011a1498a39
Alejandro Hernandez Ferriz <ahferriz@gmail.com>
2017-12-11 11:25:53 -0600

    Corrección de nombre de APP_NAME. Esqueleto clase de mensaje seguro. Actualización composer.

---
+++ app/Classes/Sistema/Mensaje.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Classes\Sistema;
+
+use Firebase\JWT\JWT;
+
+/**
+ * Clase para enviar mensajes entre sistemas
+ *
+ * @author ahfer
+ */
+class Mensaje
+{
+    //put your code here
+}
