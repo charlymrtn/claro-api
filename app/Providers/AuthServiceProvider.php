6785d39b5cfba75881572edab52625c74448086c
Alejandro Hernandez Ferriz <ahferriz@gmail.com>
2018-04-04 19:03:19 -0500

    Corrección de clase faltante Schema.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Spatie\Permission\Models\Role;
 
