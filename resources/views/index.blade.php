deca20eb67f48c430ad59b6028b69f6741464d95
vagrant <vagrant@amx-claropay-admin>
2018-05-11 20:58:12 +0000

    Remover llamadas a CDN y archivos externos en Core API

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,84 +7,28 @@
 
         <title>ClaroPay - API</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Raleway:100,600">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/fontawesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous">
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha256-ZT4HPpdCOt2lvDkXokHuhJfdOKSPFLzeAJik5U/Q+l4=" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha256-916EbMg70RQy9LHiGkXzG8hSg9EdNy97GazNG/aiY1w=" crossorigin="anonymous">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                color: #636b6f;
-                font-size: 84px;
-            }
-
-            .links {
-                color: #000;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-transform: uppercase;
-            }
-            .links > a {
-                color: #636b6f;
-                text-decoration: none;
-            }
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <script type="text/javascript" href="{{ asset('/js/mix/ui.js') }}"></script>
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/mix/ui.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/vendor.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/app.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('vendor/adminlte/plugins/iCheck/square/blue.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('vendor/adminlte/css/auth.css') }}">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
             <div class="content">
-                <div class="title m-b-md">
+                <div class="links">
                     API
                 </div>
 
-                <div class="links">
+                <div class="title m-b-md">
                     Claro Pay
                 </div>
             </div>
         </div>
-        <!-- Scripts -->
-        <script src="https://cdn.jsdelivr.net/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha256-U5ZEeKfGNOja007MMD3YBI0A3OSZOQbeG6z2f2Y0hu8=" crossorigin="anonymous"></script>
+
+       <!-- Scripts -->
+       <script src="{{ asset('vendor/adminlte/plugins/iCheck/icheck.min.js') }}"></script>
     </body>
 </html>
