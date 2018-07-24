8bdef3379e9afb47163fea595d81c53de6c7c1b7
Alejandro Hernandez Ferriz <ahferriz@gmail.com>
2018-07-24 15:17:05 -0500

    Adición de EJEMPLOS de Plan.

--- routes/api/v1/v1.php
+++ routes/api/v1/v1.php
@@ -5,14 +5,20 @@ Route::group(['namespace' => 'API\v1', 'prefix' => 'v1', 'as' => 'api.v1.'], fun
     // API v1 Principal
     Route::any('/', 'APIv1Controller@index')->name('default');
 
-    // Tarjetas
+    // API Tarjetas
     Route::group(['middleware' => ['scope:superadmin,cliente-tarjetas']], function () {
         Route::resource('/tarjeta', 'TarjetaController');
     });
 
-    // Cargos
+    // API Cargos
     Route::group(['middleware' => ['scope:superadmin,cliente-transacciones']], function () {
         Route::resource('/cargo', 'CargoController');
+    });
+
+    // API Suscripciones
+    // @todo: Cambiar permiso a: cliente-suscripciones
+    Route::group(['middleware' => ['scope:superadmin,cliente-transacciones']], function () {
+        Route::apiResource('/plan', 'PlanController');
     });
 
 });
